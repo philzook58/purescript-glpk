236eb0f700fcb4aedd94ff3b0f15a40b94a95e20
philzook58 <philzook58@gmail.com>
2018-12-15 15:24:06 -0500

    changed to foreign object

--- src/GLPK.purs
+++ src/GLPK.purs
@@ -2,12 +2,19 @@
 module GLPK where
 import Effect (Effect)
 import Data.Function.Uncurried (Fn2, runFn2, Fn3, runFn3)
+import Foreign.Object as FO
 -- import Data.StrMap
 -- foreign import pow :: Number -> Number -> Number
+
+-- purescript-foreign-object
 
 foreign import glp_create_prob :: Effect LPProb
 -- foreign import glp_read_lp_from_string :: Fn2 LPProb String 
 foreign import data LPProb :: Type
-foreign import data Result :: Type
-foreign import glpk_solve_lp :: String -> {res :: Result, obj :: Number}
-foreign import lookupVar :: Fn2 String Result Number
+--foreign import data Result :: Type
+foreign import glpk_solve_lp :: String -> {res :: FO.Object, obj :: Number}
+-- foreign import lookupVar :: Fn2 String Result Number
+
+-- lookupVar (Var s) o = FO.lookup s o  
+
+
