fe1ad4db94db81324723d5846795ca784ad84249
philzook58 <philzook58@gmail.com>
2018-12-15 10:57:21 -0500

    started actually compling main

--- src/Main.purs
+++ src/Main.purs
@@ -4,48 +4,78 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Array
 import GLPK
-{-
+
 -- a spacy concact
-(+_+) :: String -> String -> String
-x (+_+) y = x <> " " <> " " <> y 
+
+spaceAppend :: String -> String -> String
+spaceAppend x y = x <> " " <> " " <> y 
+
+-- purescript has to define infix operators using a seperate syntax
+-- +_+ is an "operator section" a weird purescipt feature
+-- it appears operators can't have characters in them
+infix 5 spaceAppend as +++
 -- a newliney concact
-(+n+) :: String -> String -> String
-x (+n+) y = x <> "\n" <> y 
+newlineAppend :: String -> String -> String
+newlineAppend x y = x <> "\n" <> y 
+
+infix 5 newlineAppend as ++:
+
 -- a plussy concact
-(+++) :: String -> String -> String
+{-
+(+++) newlineAppend :: String -> String -> String
 x (+++) y = x +_+ "+" +_+ y 
+-}
+
 
 data Var = Var String
-data LinExpr = LinExpr [(Number, Var)] -- Ax
+data LinExpr = LinExpr (Array {coeff :: Number, var :: Var})  -- Ax
 data AffineExpr = AffineExpr LinExpr Number -- Ax + b
 data Constraint = Contraint AffineExpr Ordering AffineExpr -- Ax + b <= Bx + c
 data Objective = Min LinExpr | Max LinExpr
-data Problem = Problem Objective [Constraint]
+data Problem = Problem Objective (Array Constraint)
 
+{-
+(:<=) :: AffineExpr -> AffineExpr -> Constraint
+(:==) :: AffineExpr -> AffineExpr -> Constraint
+(:<=) :: AffineExpr -> AffineExpr -> Constraint
+(:+) :: AffineExpr -> AffineExpr -> AffineExpr
+(:-) :: AffineExpr -> AffineExpr -> AffineExpr
+(:*)  :: Number -> Var -> AffineExpr
+-}
 
-(<=)
-===
-<=
-
-
-instance semigroupAffineExpr :: Semigroup AffineExpr where
+instance semigroupAffineExpr :: Semigroup LinExpr where
   append (LinExpr x) (LinExpr y) = LinExpr (x <> y)
 
 
 
-
+{-
 printProblem ::
 printProblem (Problem obj constraints) =       where
 												= printObjective obj
-
+-}
 printVar :: Var -> String
 printVar (Var s) = s
 
+{-
 printVars :: [Var] -> String
 printVars (Var s):vars = s <> "\n"
 prinvVars (Var s):[] = s
 
+-}
+
+-- You can pattern match on Lists and Sequences, but not arrays
+{-
+printVars :: List Var -> String
+printVars [] = mempty
+printVars (v:vars) = (printVar v) <> "\n" <> (printVars vars)
+-}
+printVars :: Array Var -> String
+printVars = foldMap (\v -> (printVar v) <> "\n") 
+
+
+{-
 printObjective :: Objective -> String
 printObjective (Min expr) = "Minimize" +n+ "obj:" +_+ (printLinExpr expr)
 printObjective (Max expr) = "Maximize" +n+ "obj:" +_+ (printLinExpr expr)
@@ -59,8 +89,8 @@ printAffineExpr (AffineExpr linExpr const) = (printLinExpr linExpr) +++ const
 
 printConstraint :: Constraint -> String
 printConstraint (Constraint affineExpr1 cmp affineExpr2) =  (printAffineExpr affineExpr1) +_+ (show cmp) +_+ (printAffineExpr affineExpr2)
-
 -}
+
 
 
 main :: Effect Unit
